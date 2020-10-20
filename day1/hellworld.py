682f4fd9c036e7dff0173bccb2ab7e9f0d93ff66
Neo Xie <neo.xie@ksb.com>
2020-10-20 08:41:36 +0800

    refactor

--- day1/hellworld.py
+++ day1/hellworld.py
@@ -1,12 +1,13 @@
 import sys
 
 
-def main():
-    print("hello " + sys.argv[1])
+def hello(name):
+    print("hello " + name)
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("please input your name, and try again, e.g. python helloworld.py <your name>")
-        exit()
-    main()
+        print(
+            "please input your name, and try again, e.g. python helloworld.py <your name>")
+    else:
+        hello(sys.argv[1])
